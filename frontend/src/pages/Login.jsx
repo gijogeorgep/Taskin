bec2c50b93de8fb3d3d9fec2816d96caab0da0ed
gijo <gijogeorgep02@gmail.com>
2025-08-16 18:51:00 +0530

    update: changes in login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -21,10 +21,11 @@ function Login() {
       console.error("Login failed:", error);
     }
   };
+
   return (
-    <div className="bg-[#1F2937] min-h-screen flex items-center justify-center py-10 px-4">
-      <div className="bg-gray-900 w-full max-w-md rounded-md  overflow-hidden p-6 md:p-10 flex flex-col justify-center items-center gap-6">
-        <div className="text-center text-white">
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center py-10 px-4">
+      <div className="bg-white w-full max-w-md rounded-md shadow-md overflow-hidden p-6 md:p-10 flex flex-col justify-center items-center gap-6">
+        <div className="text-center text-gray-800">
           <h2 className="text-2xl sm:text-3xl font-bold mb-2">Welcome Back</h2>
           <p className="text-base sm:text-lg opacity-80">Access your account</p>
         </div>
@@ -34,25 +35,25 @@ function Login() {
           className="w-full flex flex-col items-center gap-4"
         >
           <input
-            className="w-full max-w-md p-3 rounded-md bg-white text-black"
+            className="w-full max-w-md p-3 rounded-md border border-gray-300 bg-white text-gray-800"
             type="email"
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           <input
-            className="w-full max-w-md p-3 rounded-md bg-white text-black"
+            className="w-full max-w-md p-3 rounded-md border border-gray-300 bg-white text-gray-800"
             type="password"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <Link to="/forgot-password" className="text-blue-500 hover:underline">
+          <Link to="/forgot-password" className="text-blue-600 hover:underline">
             Forgot password?
           </Link>
           <button
             type="submit"
-            className="bg-gray-950 hover:bg-gray-800 text-white px-8 py-3 rounded-md text-center w-full max-w-md"
+            className="bg-blue-600 hover:bg-blue-500 text-white px-8 py-3 rounded-md text-center w-full max-w-md"
           >
             Submit
           </button>
